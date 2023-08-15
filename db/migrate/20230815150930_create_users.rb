25d044797a294577fb17bcf3eb1eff28a52d5857
Theodore Asimeng-Osei <theodoraldo@gmail.com>
2023-08-15 15:45:51 +0000

    Generate migration for users table

--- db/migrate/20230815150930_create_users.rb
+++ db/migrate/20230815150930_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :Name
-      t.bytea :Photo
+      t.oid :Photo
       t.text :Bio
       t.integer :PostCounter
 
