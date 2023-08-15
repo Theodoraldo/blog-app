ad687ad48c6f92e8a45944aa235a0630e55821f5
Theodore Asimeng-Osei <theodoraldo@gmail.com>
2023-08-15 18:14:33 +0000

    Run migration to update schema.rb file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_15_171641) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_15_173536) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_15_171641) do
     t.integer "LikesCounter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -49,4 +51,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_15_171641) do
 
   add_foreign_key "comments", "users"
   add_foreign_key "likes", "users"
+  add_foreign_key "posts", "users", column: "author_id"
 end
