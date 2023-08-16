68e882a8aea19eb8cd572c8b697935f82ef02a5c
Theodore Asimeng-Osei <theodoraldo@gmail.com>
2023-08-16 19:33:44 +0000

    Insert queries

--- db/migrate/20230815150930_create_users.rb
+++ db/migrate/20230815150930_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :Name
-      t.oid :Photo
-      t.text :Bio
-      t.integer :PostCounter
+      t.string :name
+      t.oid :photo
+      t.text :bio
+      t.integer :postCounter
 
       t.timestamps
     end
--- db/migrate/20230815164609_create_posts.rb
+++ db/migrate/20230815164609_create_posts.rb
@@ -1,10 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.string :Title
-      t.text :Text
-      t.integer :CommentsCounter
-      t.integer :LikesCounter
+      t.string :title
+      t.text :text
+      t.integer :commentscounter
+      t.integer :likescounter
 
       t.timestamps
     end
