a1b7fff1513ecd9a4869d1a4c463da950d8ba6b8
Theodore Asimeng-Osei <theodoraldo@gmail.com>
2023-08-17 18:36:27 +0000

    Add validation to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 250, too_long: '%{ count } characters is the maximum allowed' }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   after_create :counter_posts_update
   has_many :likes
   has_many :comments
