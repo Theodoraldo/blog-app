058b8868cffee9078c93ccd59e1b3741a34ed906
Theodore Asimeng-Osei <theodoraldo@gmail.com>
2023-08-17 02:02:29 +0000

    Add methods to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
   belongs_to :user, foreign_key: 'author_id'
+
+  def update_posts_counter
+    author.increment!(:posts_counter)
+  end
+
+  def recent_comments(id)
+    Comment.where(post_id: id).order(created_at: :desc).limit(5)
+  end
 end
