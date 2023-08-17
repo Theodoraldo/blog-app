db67433139fc649b9ce6bbf727dfb8c6b6613b80
Theodore Asimeng-Osei <theodoraldo@gmail.com>
2023-08-17 01:26:12 +0000

    Add method in User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :comments
   has_many :likes
+
+  def recent_posts(id)
+    Post.where(author_id: id).order(created_at: :desc).limit(3)
+  end
 end
