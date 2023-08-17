a39f56051278267902bef47af14f8fab2a6e5cb7
Theodore Asimeng-Osei <theodoraldo@gmail.com>
2023-08-17 02:10:18 +0000

    Add methods to comment class

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  def update_comment_counter(post_id)
+    Post.increment_counter(:comments_counter, post_id)
+  end
 end
