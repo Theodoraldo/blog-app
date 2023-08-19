3bd72f33a193cf0b5c3e9846e42d5538424e83e6
Theodore Asimeng-Osei <theodoraldo@gmail.com>
2023-08-19 07:13:12 +0000

    Modify classes and methods

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,7 +3,7 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  def update_likes_counter(post_id)
-    Post.increment_counter(:likes_counter, post_id)
+  def update_likes_counter
+    post.increment!(:likes_counter)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,16 +2,15 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 250, too_long: '%{ count } characters is the maximum allowed' }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  after_create :counter_posts_update
   has_many :likes
   has_many :comments
   belongs_to :user, foreign_key: 'author_id'
 
-  def counter_posts_update(id)
-    User.increment_counter(:posts_counter, id)
+  def counter_posts_update
+    user.update(post_counter: user.posts.count)
   end
 
-  def recent_comments(id)
-    Comment.where(post_id: id).order(created_at: :desc).limit(5)
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
   end
 end
