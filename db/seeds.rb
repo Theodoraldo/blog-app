c0737137b951793790fb7b779515ef7af5f034e5
Theodoraldo <theodoraldo@gmail.com>
2023-08-23 13:51:49 +0000

    Modify schema file and seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,60 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+# Clear existing data
+Comment.delete_all
+Like.delete_all
+Post.delete_all
+User.delete_all
 
-first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
-second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+# Users
+user1 = User.create!(
+  name: "Theodoraldo Gishun",
+  photo: "https://unsplash.com/photos/F_-0BxGuVvo",
+  bio: "This is User 1's bio. A developer from Poland, specialize in rails.",
+  post_counter: 0
+)
+user2 = User.create!(
+  name: "Asimeng Osei",
+  photo: "https://unsplash.com/photos/F_-0BxGuVvo",
+  bio: "This is User 2's bio. A developer from Ghana, specialize in react.",
+  post_counter: 0
+)
 
+# Posts
+post1 = Post.create!(
+  title: "Post 1",
+  text: "This is the content of Post 1.",
+  comments_counter: 0,
+  likes_counter: 0,
+  author_id: user1.id
+)
+post2 = Post.create!(
+  title: "Post 2",
+  text: "This is the content of Post 2.",
+  comments_counter: 0,
+  likes_counter: 0,
+  author_id: user2.id
+)
 
-first_post = Post.create(author_id: 1, title: 'Hello', text: 'This is my first post')
-second_post = Post.create(author_id: 2, title: 'Hi', text: 'This is my second post')
-third_post = Post.create(author_id: 1, title: 'Peer', text: 'This is my third post')
-forth_post = Post.create(author_id: 1, title: 'Dude', text: 'This is my fourth post')
-fifth_post = Post.create(author_id: 2, title: 'Heh', text: 'This is my fifth post')
+# Comments
+Comment.create!(
+  text: "This is a comment on Post 1.",
+  user: user2,
+  post: post1
+)
+Comment.create!(
+  text: "This is another comment on Post 1.",
+  user: user1,
+  post: post1
+)
 
-Comment.create(post_id: 1, user_id: 1, text: 'Hi Tom!' )
-Comment.create(post_id: 2, user_id: 2, text: 'Hi Tom!' )
-Comment.create(post_id: 3, user_id: 1, text: 'Hi Tom!' )
-Comment.create(post_id: 4, user_id: 2, text: 'Hi Tom!' )
-Comment.create(post_id: 5, user_id: 1, text: 'Hi Tom!' )
-Comment.create(post_id: 1, user_id: 2, text: 'Hi Tom!' )
+# Likes
+Like.create!(
+  user: user1,
+  post: post2
+)
+Like.create!(
+  user: user2,
+  post: post2
+)
