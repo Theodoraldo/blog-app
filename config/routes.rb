56c7079caf92f3be9b9122b6a771302f810182b8
Theodoraldo <theodoraldo@gmail.com>
2023-08-24 10:46:31 +0000

    Change root route of application

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'users#index'
   # get "/users/:author_id/posts", to: "posts#index"
 
   # get "/users/:author_id/posts/:id", to: "posts#show"
